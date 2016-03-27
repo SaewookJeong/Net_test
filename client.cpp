56f7d881fd777d8fb85bdfe93beb9a4a94d01543
SaewookJeong <tpdnrzz@jungbu.ac.kr>
2016-03-27 14:33:55 -0700

    Thread File

--- client.cpp
+++ client.cpp
@@ -1,81 +1,91 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<winsock2.h>
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include<string.h>
+#include<process.h> //스레드를 위한 헤더파일
+ 
 using namespace std;
-   
+
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+   SOCKET clientsock = (SOCKET)arg;
+   char sendmessage[1024];
+   SOCKADDR_IN clientinfo;
+   int clientsize;
+   int strlen; //recv 리턴 값 저장 할 변수 선언
+   clientsize = sizeof(clientinfo); 
+   getpeername(clientsock, (SOCKADDR*)&clientinfo, &clientsize);
+   //함수는 소켓이 연결되어 있는 원격지 상대방(peer)의 이름(인터페이스 어드레스와 포트번호)을 얻는 함수입니다.
+ 
+	strlen=recv(clientsock , sendmessage, sizeof(sendmessage), 0);
+	if(strlen == -1) cout<<"메시지 수신 실패"<<endl;
+	cout<<sendmessage<<endl;
+	return 0;
+	ExitThread((DWORD)ProcessClient);
+}
+
 int main(int argc, char *argv[])
 {
-   
-	SOCKET clientsock;
+	SOCKET clientsock; // 클라이언트 프로그램 소켓 생성
 	WSADATA wsa;
-	struct sockaddr_in sockinfo;
+	struct sockaddr_in sockinfo; // 서버 연결 할 주소 구조체 생성
+ 
+	char id[10] = "Client";
 	char sendmessage[1024];
-	char revmessage[1024];
-   	char message[30];
-	char message1[]="disconnected";
-	char id2[]="Client";
-	char id[30];
-	int strlen;
-   
-	if(argc!=3)
+	int result; // 결과 값
+	
+	if (argc != 3)
 	{
-	   printf("Usage : %s <IP> <PORT> \n",argv[0]);
-	   exit(1);
-	} 
+		printf("Usage : %s <IP> <PORT> \n", argv[0]);
+		exit(1);
+	}
 
 	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
-	printf("초기화 실패\n");
-   
-	clientsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP) ;
-   
+		cout<<"초기화 실패\n"<<endl;
+
+	clientsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
 	if(clientsock == INVALID_SOCKET)
-		printf("소켓 생성 실패\n");
-   
-	memset(&sockinfo,0,sizeof(sockinfo));
-   
+		cout<<"소켓 생성 실패\n"<<endl;
+
+	memset(&sockinfo, 0, sizeof(sockinfo)); // 주소 구조체 함수 초기화
+  
 	sockinfo.sin_family = AF_INET;
-	sockinfo.sin_port =htons (atoi(argv[2]));
-	sockinfo.sin_addr.s_addr =inet_addr(argv[1]) ;
-   
-	if(connect(clientsock,(SOCKADDR*)&sockinfo,sizeof(sockinfo))==SOCKET_ERROR) // 서버 측에 연결 요청
-	printf(" 서버 접속 실패 ");
+	sockinfo.sin_port =htons(atoi(argv[2]));
+	sockinfo.sin_addr.s_addr =inet_addr(argv[1]);
 
-	strlen=recv(clientsock , message , sizeof(message)-1 , 0 );
-	if(strlen==-1)
-	printf(" 메세지 수신 실패 ");
-	printf(" Server say: %s \n",message);
+	result = connect(clientsock, (SOCKADDR*)&sockinfo, sizeof(sockinfo)); // return 값은 0이다.  그 외에는 SOCKET_ERROR 값(-1)을 리턴한다.
+	if(result == -1 || result == SOCKET_ERROR)
+		cout<<"서버측 connect(서버 접속) 요청에 실패하였습니다"<<endl;
+	//cout<<result; // 여기서 결과 값이 -1이면 연결이 되지 않는 것
 
-	while(1){
+	//strlen=recv(clientsock , message, sizeof(message)-1 , 0); // success = return value is zero. Otherwise = SOCKET_ERROR
+	//if(strlen == -1) cout<<"메시지 수신 실패"<<endl;
+	cout<<"Connect"<<endl;
+
+	HANDLE hThread;      // 스레드 핸들
+	DWORD ThreadID;     // 스레드 아이디
 	
-		recv(clientsock, id, sizeof(id), 0);
-		cout<<" "<<id;
+	while(1){
+	cin>>sendmessage;
+	if((strcmp(sendmessage, "quit") == 0))
+	{
+		cout<<"Disconnect"<<endl;
+		break;
+	}
+	send(clientsock, sendmessage, sizeof(sendmessage)-1, 0);	
+	
+	
+	hThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)clientsock, 0, &ThreadID);
+	}
 
-		recv(clientsock, revmessage, sizeof(revmessage), 0);
-		if(strncmp(revmessage, "quit", 4) == 0)
-		{
-			closesocket(clientsock);		
-			printf("%s", message1);
-			goto end;
-		}
+	
+	
+	closesocket(clientsock);
+ 
+	WSACleanup(); //윈속 종료
 
-		cout<<": "<<revmessage<<endl;
-		cout<<" "<<id2<<": ";
-		cin>>sendmessage;
-		if(strncmp(sendmessage, "quit", 4) == 0)
-		{
-			closesocket(clientsock);		
-			printf("%s", message1);
-			goto end;
-		} 
-
-		send(clientsock, id2, sizeof(id2), 0);
-		send(clientsock, sendmessage, sizeof(sendmessage), 0);
-		char revmessage[1024]={0};
-		char sendmessage[1024]={0};
-		}
-
-end:
-	WSACleanup();
+	return 0;
+ 
 }
+
--- server (3).cpp
+++ server (3).cpp
@@ -1,91 +1,98 @@
 #include<stdio.h>
 #include<winsock2.h>
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include<process.h> //스레드를 위한 헤더파일
 using namespace std;
-#define BUFSIZE 512;
+ 
+char echo[10];
+
+DWORD WINAPI ProcessClient(LPVOID arg)
+{
+   SOCKET clientsock = (SOCKET)arg;
+   char revmessage[1024];
+   SOCKADDR_IN clientinfo;
+   int clientsize;
+   int strlen;
+   clientsize = sizeof(clientinfo);
+   getpeername(clientsock, (SOCKADDR*)&clientinfo, &clientsize);
+   //함수는 소켓이 연결되어 있는 원격지 상대방(peer)의 이름(인터페이스 어드레스와 포트번호)을 얻는 함수입니다.
+  
+   if(clientsock==INVALID_SOCKET) cout<<"클라이언트 소켓 연결 실패 "<<endl;
+	cout<<"Connect"<<endl;
+		while(1){
+		for(int i = 0; i < 1024; i ++) revmessage[i]=0;
+		strlen=recv(clientsock , revmessage, sizeof(revmessage)-1, 0);
+		if(strlen == 0) 
+		{
+			cout<<"Disconnect"<<endl;
+			break;
+		}
+			cout<<revmessage<<endl;
+			if(strcmp(echo, "-echo") == 0)
+			send(clientsock, revmessage, sizeof(revmessage)-1, 0);
+		}
+	return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
-		SOCKET sock,clientsock;
-		WSADATA wsa;
-		struct sockaddr_in sockinfo, clientinfo;
-		int clientsize;
-		char message[]="connected";
-		char message1[]="disconnected";
-		char	id[]="server";
-		char id2[30];
-		char sendmessage[1024];
-		char revmessage[1024];
-		int len;
+	SOCKET sock, clientsock;
+	WSADATA wsa;
+	struct sockaddr_in sockinfo, clientinfo;
+	int clientsize;
+	char message[]="Connect";
 
-		if(argc != 2){
-		printf("Usage : %s <port> <-echo>\n", argv[0]);
+	if (argc != 2)
+	{
+		if(argc == 3 && strcmp(argv[2], "-echo") == 0)
+		{
+			strcpy(echo, argv[2]);
+			goto start;
+		}
+		printf("Usage : %s <PORT> <-echo> \n", argv[0]);
 		exit(1);
-		}
-		
-		
-    
-		if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
-		printf("초기화 실패\n");
-   
-		sock = socket( AF_INET , SOCK_STREAM , IPPROTO_TCP ) ;
-   
-		if(sock == INVALID_SOCKET)
-		printf("소켓 생성 실패\n");
-   
-		memset(&sockinfo,0,sizeof(sockinfo));
-   
-		sockinfo.sin_family = AF_INET;
-		sockinfo.sin_port =htons (atoi(argv[1]));
-		sockinfo.sin_addr.s_addr = htonl ( INADDR_ANY );    
-   
-		if ( bind ( sock , (SOCKADDR*)&sockinfo , sizeof(sockinfo) ) == SOCKET_ERROR )
-		printf(" bind 실패 " );
-	   
-		if ( listen ( sock , 5 )==SOCKET_ERROR )
-		printf(" 대기열 실패 ");
-   
-		clientsize = sizeof(clientinfo);
-   
-		printf("\n클라이언트로부터 접속을 기다리고 있습니다...\n\n");
- 
-		clientsock = accept(sock, (SOCKADDR*)&clientinfo, &clientsize);
-   
-		if(clientsock==INVALID_SOCKET)
-		printf(" 클라이언트 소켓 연결 실패 ");
-
-		send(clientsock, message, sizeof(message), 0 );
-		cout<<"접속이 이루어 졌습니다\n";
-
-
-		while(1){	
-		cout<<" "<<id<<": "; 
-		cin>>sendmessage;
-		if(strncmp(sendmessage, "quit", 4) == 0)
-		{
-			closesocket(sock);
-			closesocket(clientsock);		
-			printf("%s", message1);
-			break;
-		}
-		len = strlen(sendmessage);
-		if(sendmessage[len-1] == '\n')
-			sendmessage[len-1] = '\0';
-		send(clientsock, id, sizeof(id), 0);
-		send(clientsock, sendmessage, sizeof(sendmessage), 0);
-		recv(clientsock, id2, sizeof(id2), 0);
-		cout<<" "<<id2;
-		recv(clientsock, revmessage, sizeof(revmessage), 0);
-		cout<<": "<<revmessage<<endl;
-		if(strncmp(revmessage, "quit", 4) == 0)
-		{
-			char revmessage[1024]={0};
-			char sendmessage[1024]={0};
-			closesocket(clientsock);		
-			printf("%s", message1);
-			break;
-		}
 	}
-WSACleanup(); 
+
+start:
+	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+		cout<<"초기화 실패"<<endl;
+
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(sock == INVALID_SOCKET)
+		cout<<"소켓 생성 실패"<<endl;
+
+	memset(&sockinfo,0,sizeof(sockinfo));
+
+	sockinfo.sin_family = AF_INET;
+	sockinfo.sin_port = htons(atoi(argv[1]));
+	sockinfo.sin_addr.s_addr=htonl(INADDR_ANY);
+  
+	if(bind(sock, (SOCKADDR*)&sockinfo, sizeof(sockinfo)) == SOCKET_ERROR) // return 값 success = 0, otherwise = SOCKET_ERROR;
+		cout<<"bind 실패"<<endl;
+
+	if(listen(sock , 5) == SOCKET_ERROR) // return 값 success = 0, otherwise = SOCKET_ERROR;
+		cout<<"대기열 실패 "<<endl;
+	
+	clientsize = sizeof(clientinfo);
+	cout<<"클라이언트로부터 접속을 기다리고 있습니다.."<<endl;
+
+
+	HANDLE hThread;      // 스레드 핸들
+	DWORD ThreadID;     // 스레드 아이디
+		
+	while(1) 
+	{
+		clientsock = accept(sock, (SOCKADDR*)&clientinfo, &clientsize);
+		
+		hThread = CreateThread(NULL, 0, ProcessClient, (LPVOID)clientsock, 0, &ThreadID);
+
+		if(hThread == NULL)cout<<"스레드 생성 실패"<<endl;
+
+		else CloseHandle(hThread);
+    }	
+	closesocket(clientsock);
+	closesocket(sock);
+	WSACleanup();
 }
